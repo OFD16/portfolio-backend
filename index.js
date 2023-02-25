3fc9431f622f7a3ef3850bdb333f8113e0ab65d7
Ömer Faruk Demirsoy <omerfaruk.demirsoy@agu.edu.tr>
2023-02-25 14:29:39 +0300

    tryig again

--- index.js
+++ index.js
@@ -12,9 +12,9 @@ const server = express();
 
 server.use(express.json()); //gelen bütün istekleri json formatında dönderir gibi bir şey
 
-server.use(`/users`, user_router);
+server.use("/users", user_router);
 
-server.post(`/login`, (req, res, next) => {
+server.post("/login", (req, res, next) => {
     const email = req.body.sign_mail;
     const password = helper.crypto(req.body.password);
     console.log("şifre: " ,password);
@@ -23,8 +23,8 @@ server.post(`/login`, (req, res, next) => {
     const tokenTime = auth.checkTokenTime(token);
     users.login(email, password)
         .then((user) => {
-            server.use(`/blogs`, post_router);
-            server.use(`/projects`, project_router);
+            server.use("/blogs", post_router);
+            server.use("/projects", project_router);
             res.status(200).json({
                 statusCode: 200,
                 response: "Kullanıcı başarıyla giriş yaptı!",
